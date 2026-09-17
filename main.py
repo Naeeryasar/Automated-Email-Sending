754670c5ec9616b92c0239a543d2707cb2e9d86c
Naeeryasar <naeeryasar3486@gmail.com>
2026-09-17 17:38:34 +0600

    Email details

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+sender_email = "alf.stoltenberg21@ethereal.email"
+sender_password = "YOUR_PASSWORD"
+receiver_email = "naeeryasar3486@gmail.com"
