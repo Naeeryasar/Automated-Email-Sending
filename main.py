4827c895c7dea0c8365603b93deeba93309e1b2e
Naeeryasar <naeeryasar3486@gmail.com>
2026-09-17 17:41:55 +0600

    Connecting and login

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 sender_email = "alf.stoltenberg21@ethereal.email"
-sender_password = "YOUR_PASSWORD"
+sender_password = "6kFGCmFVUnU3RDjBmP"
 receiver_email = "naeeryasar3486@gmail.com"
 
 subject = "Automated Email Test"
@@ -14,3 +14,8 @@ message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
 message.attach(MIMEText(body, "plain"))
+
+server = smtplib.SMTP("smtp.ethereal.email", 587)
+server.starttls()
+server.login(sender_email, sender_password)
+
